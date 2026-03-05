e6fe30001900deb7c556de2c14c267251ee91c31
rayner006 <ifadarayner007@aol.com>
2026-03-05 08:31:33 +0100

    Create client.js

---
+++ src/core/client.js
@@ -0,0 +1,19 @@
+/**
+ * Discord Client Setup
+ */
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.DirectMessageReactions
+    ]
+});
+
+module.exports = { client };
